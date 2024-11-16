80ae53e6e2a114117c9c25ddbe7811955e0ef90c
Solymosi Zoltán István <57014842+TavstalDev@users.noreply.github.com>
2024-11-16 17:29:33 +0100

    Initial commit

---
+++ TExample/ExampleConfig.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Tavstal.TExample.Models;
+using Tavstal.TLibrary.Models.Plugin;
+
+namespace Tavstal.TExample
+{
+    public class ExampleConfig : ConfigurationBase
+    {
+        [JsonProperty(Order = 3)]
+        public DatabaseData Database { get; set; }
+
+        public override void LoadDefaults()
+        {
+            DebugMode = false;
+            Locale = "en";
+            DownloadLocalePacks = true;
+            Database = new DatabaseData("example_players");
+        }
+
+        // Required because of the library
+        public ExampleConfig() { }
+        public ExampleConfig(string fileName, string path) : base(fileName, path) { }
+    }
+}
