739202d0fca614bc4d62f29131d95f65c6e912a3
Tavstal <tavstal@gmail.com>
2024-11-19 10:28:39 +0100

    Implemented player join and left events

--- TTrade/Handlers/PlayerEventHandler.cs
+++ TTrade/Handlers/PlayerEventHandler.cs
@@ -1,5 +1,8 @@
-﻿using Rocket.Unturned;
+﻿using System.Collections.Generic;
+using Rocket.Unturned;
 using Rocket.Unturned.Player;
+using Tavstal.Trade.Components;
+using Tavstal.Trade.Managers;
 using Tavstal.Trade.Models;
 
 namespace Tavstal.Trade.Handlers
@@ -7,6 +10,9 @@ namespace Tavstal.Trade.Handlers
     public static class PlayerEventHandler
     {
         private static bool _isAttached;
+        // ReSharper disable once InconsistentNaming
+        private static readonly List<UnturnedPlayer> _players = new List<UnturnedPlayer>();
+        public static List<UnturnedPlayer> Players => _players;
 
         public static void AttachEvents()
         {
@@ -16,6 +22,7 @@ namespace Tavstal.Trade.Handlers
             _isAttached = true;
 
             U.Events.OnPlayerConnected += OnPlayerConnected;
+            U.Events.OnPlayerDisconnected += OnPlayerDisconnected;
         }
 
         public static void DetachEvents()
@@ -26,11 +33,22 @@ namespace Tavstal.Trade.Handlers
             _isAttached = false;
 
             U.Events.OnPlayerConnected -= OnPlayerConnected;
+            U.Events.OnPlayerDisconnected -= OnPlayerDisconnected;
+        }
+        
+        private static void OnPlayerConnected(UnturnedPlayer player)
+        {
+            _players.Add(player);
         }
 
-        private static async void OnPlayerConnected(UnturnedPlayer player)
+        private static void OnPlayerDisconnected(UnturnedPlayer player)
         {
-           
+            _players.Remove(player);
+           TradeComponent component = player.GetComponent<TradeComponent>();
+           if (component.State == ETradeState.None)
+               return;
+
+           TradeManager.CancelTrade(player);
         }
     }
 }
