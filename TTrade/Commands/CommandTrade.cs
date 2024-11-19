af4fbcd1b00bafef5da8ec548d3d43a043c65bd7
Tavstal <tavstal@gmail.com>
2024-11-19 10:29:21 +0100

    Implemented subcommands

--- TTrade/Commands/CommandTrade.cs
+++ TTrade/Commands/CommandTrade.cs
@@ -2,10 +2,14 @@
 using Rocket.Unturned.Player;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Steamworks;
 using Tavstal.TLibrary.Models.Commands;
 using Tavstal.TLibrary.Models.Plugin;
 using Tavstal.TLibrary.Helpers.Unturned;
+using Tavstal.Trade.Components;
 using Tavstal.Trade.Managers;
+using Tavstal.Trade.Models;
+using UnityEngine;
 
 namespace Tavstal.Trade.Commands
 {
@@ -15,14 +19,14 @@ namespace Tavstal.Trade.Commands
         public override AllowedCaller AllowedCaller => AllowedCaller.Player;
         public override string Name => "trade";
         public override string Help => "Securely exchange items with other players through a simple request and confirmation process.";
-        public override string Syntax => "[player] | accept | deny | done | open | view | cancel";
+        public override string Syntax => "[player] | accept | deny | open | view | finish | cancel";
         public override List<string> Aliases => new List<string>() { "deal", "swap", "barter" };
         public override List<string> Permissions => new List<string> { "ttrade.command.trade" };
 
         // 'help' subcommand is built-in, you don't need to add it
         protected override List<SubCommand> SubCommands => new List<SubCommand>()
         {
-            new SubCommand("accept", "", "accept [player]", new List<string>() { "allow" }, new List<string>() { "ttrade.command.trade.accept" }, 
+            new SubCommand("accept", "Accept a pending trade request.", "accept [player]", new List<string>() { "allow", "approve" }, new List<string>() { "ttrade.command.trade.accept" }, 
                 (caller,  args) =>
                 {
                     if (args.Length != 1)
@@ -39,10 +43,10 @@ namespace Tavstal.Trade.Commands
                         return Task.CompletedTask;
                     }
 
-                    //VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+                    TradeManager.AcceptTradeRequest(callerPlayer, targetPlayer);
                     return Task.CompletedTask;
                 }),
-            new SubCommand("deny", "", "deny [player]", new List<string>() { "disallow" }, new List<string>() { "ttrade.command.trade.deny" }, 
+            new SubCommand("deny", "Decline a trade request.", "deny [player]", new List<string>() { "disallow", "disapprove" }, new List<string>() { "ttrade.command.trade.deny" }, 
                 (caller,  args) =>
                 {
                     if (args.Length != 1)
@@ -59,10 +63,10 @@ namespace Tavstal.Trade.Commands
                         return Task.CompletedTask;
                     }
 
-                    //VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+                    TradeManager.DenyTradeRequest(callerPlayer, targetPlayer);
                     return Task.CompletedTask;
                 }),
-            new SubCommand("done", "", "done", new List<string>() { "finish", "complete" }, new List<string>() { "ttrade.command.trade.done" }, 
+            new SubCommand("finish", "Complete, then finalize the trade.", "finish", new List<string>() { "done", "complete" }, new List<string>() { "ttrade.command.trade.finish" }, 
                 (caller,  args) =>
                 {
                     if (args.Length != 0)
@@ -72,10 +76,26 @@ namespace Tavstal.Trade.Commands
                     }
 
                     UnturnedPlayer callerPlayer = (UnturnedPlayer)caller;
-                    //VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+                    TradeComponent comp = callerPlayer.GetComponent<TradeComponent>();
+                    if (comp.State == ETradeState.None)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_not_in_trade");
+                        return Task.CompletedTask;
+                    }
+
+                    if (comp.State == ETradeState.Finished)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_wait_partner_finish");
+                        return Task.CompletedTask;
+                    }
+
+                    if (comp.State == ETradeState.Approved)
+                        TradeManager.FinishTrade(callerPlayer);
+                    else
+                        TradeManager.ApproveTrade(callerPlayer);
                     return Task.CompletedTask;
                 }),
-            new SubCommand("open", "", "open", new List<string>(), new List<string>() { "ttrade.command.trade.open" }, 
+            new SubCommand("open", "Open the trade inventory.", "open", new List<string>(), new List<string>() { "ttrade.command.trade.open" }, 
                 (caller,  args) =>
                 {
                     if (args.Length != 0)
@@ -85,10 +105,38 @@ namespace Tavstal.Trade.Commands
                     }
 
                     UnturnedPlayer callerPlayer = (UnturnedPlayer)caller;
-                    //VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+                    TradeComponent comp = callerPlayer.GetComponent<TradeComponent>();
+                    
+                    if (comp.State == ETradeState.None)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_not_in_trade");
+                        return Task.CompletedTask;
+                    }
+
+                    UnturnedPlayer tradePartner = UnturnedPlayer.FromCSteamID((CSteamID)comp.TradePartner);
+                    if (tradePartner == null)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_trade_partner_not_found");
+                        return Task.CompletedTask;
+                    }
+                    
+                    float distance = Vector3.Distance(callerPlayer.Position, tradePartner.Position);
+                    if (distance > TTrade.Instance.Config.Distance)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_trade_distance", distance, TTrade.Instance.Config.Distance);
+                        return Task.CompletedTask;
+                    }
+
+                    if (comp.State == ETradeState.Approved || comp.State == ETradeState.Finished)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_finished_trade_part");
+                        return Task.CompletedTask;
+                    }
+                    
+                    VaultManager.OpenVault(callerPlayer, comp.VaultId, true);
                     return Task.CompletedTask;
                 }),
-            new SubCommand("view", "", "view", new List<string>() { "check" }, new List<string>() { "ttrade.command.trade.view" }, 
+            new SubCommand("view", "View the other player's trade inventory.", "view", new List<string>() { "check" }, new List<string>() { "ttrade.command.trade.view" }, 
                 (caller,  args) =>
                 {
                     if (args.Length != 0)
@@ -96,12 +144,35 @@ namespace Tavstal.Trade.Commands
                         ExecuteHelp(caller, true, "view", args);
                         return Task.CompletedTask;
                     }
+                    
 
                     UnturnedPlayer callerPlayer = (UnturnedPlayer)caller;
-                    //VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+                    TradeComponent comp = callerPlayer.GetComponent<TradeComponent>();
+
+                    if (comp.State == ETradeState.None)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_not_in_trade");
+                        return Task.CompletedTask;
+                    }
+
+                    UnturnedPlayer tradePartner = UnturnedPlayer.FromCSteamID((CSteamID)comp.TradePartner);
+                    if (tradePartner == null)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_trade_partner_not_found");
+                        return Task.CompletedTask;
+                    }
+
+                    TradeComponent partnerComp = tradePartner.GetComponent<TradeComponent>();
+                    float distance = Vector3.Distance(callerPlayer.Position, tradePartner.Position);
+                    if (distance > TTrade.Instance.Config.Distance)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_trade_distance", distance, TTrade.Instance.Config.Distance);
+                        return Task.CompletedTask;
+                    }
+                    VaultManager.OpenVault(callerPlayer, partnerComp.VaultId, false);
                     return Task.CompletedTask;
                 }),
-            new SubCommand("cancel", "", "cancel", new List<string>() { "quit" }, new List<string>() { "ttrade.command.trade.cancel" }, 
+            new SubCommand("cancel", "Cancel an ongoing trade.", "cancel", new List<string>() { "quit" }, new List<string>() { "ttrade.command.trade.cancel" }, 
                 (caller,  args) =>
                 {
                     if (args.Length != 0)
@@ -111,7 +182,14 @@ namespace Tavstal.Trade.Commands
                     }
 
                     UnturnedPlayer callerPlayer = (UnturnedPlayer)caller;
-                    //VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+                    TradeComponent comp = callerPlayer.GetComponent<TradeComponent>();
+                    if (comp.State == ETradeState.None)
+                    {
+                        TTrade.Instance.SendCommandReply(callerPlayer, "error_not_in_trade");
+                        return Task.CompletedTask;
+                    }
+                    
+                    TradeManager.CancelTrade(callerPlayer);
                     return Task.CompletedTask;
                 })
         };
@@ -132,7 +210,7 @@ namespace Tavstal.Trade.Commands
                 return Task.FromResult(true);
             }
 
-            VaultManager.SendTradeRequest(callerPlayer, targetPlayer);
+            TradeManager.SendTradeRequest(callerPlayer, targetPlayer);
             return Task.FromResult(true);
         }
     }
